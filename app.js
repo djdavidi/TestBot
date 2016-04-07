83b8dfd39335a260ceb5ce81ace14f2b5cadc73f
[David Scherban] <davidscherban@gmail.com>
2016-04-07 00:13:40 -0400

    refactored to make usable for both mobile and desktop

--- app.js
+++ app.js
@@ -1,69 +1,69 @@
 var CronJob = require('cron').CronJob;
 var config = require('./config')
 var Nightmare = require('nightmare');
-var rp = require('request-promise');
-var users = config.users;
-var url = config.url;
-var homeUrl = config.homeUrl;
 var randomNumber;
 var user;
 var words;
-var nightmare = new Nightmare({
-        show: true,
-        'webPreferences': {
-            partition: 'nopersist'
-        }
-    })
 
 var job = new CronJob('* * 10-18/2 * *', function() {
-	var d = new Date();
-    if (d.getHours() === 10) randomNumber=null;
-    if(!randomNumber)randomNumber = Math.floor(Math.random()* (5));
     user = users[randomNumber]
     console.log(user)
     action(user);
     randomNumber++
-    if(randomNumber>=users.length) randomNumber=0;
+    if (randomNumber >= users.length) randomNumber = 0;
 
 }, null, true).start();
-
-var action = function(user){
-console.log(user)
-//wordnik random list api
-rp(config.wordsEndpoint)
-    .then(function(wordList) {
-        wordList = JSON.parse(wordList)
-        words = [];
-        wordList.forEach(function(item) {
-            words.push(item.word)
-        })
-        console.log("words" + words)
-    })
-    .then(function() {
-        nightmare
-            .goto(config.url)
-            .click('span a')
-            .wait(1500)
-            .type('input[type="email"]', user.email)
-            .type('input[type="password"]', user.password)
-            .click(config.signInSelector)
-            .wait(1300)
-            .click(config.moveSelector)
-            .wait(1400)
-        for (var i = 0; i < words.length; i++) {
-            nightmare
-                .insert('input[type="search"]', '')
-                .type('input[type="search"]', words[i])
-                .wait(2000)
-                .click(config.searchSelector)
-                .wait(Math.random()*10000)
-        }
-        nightmare
-            .wait(3000)
-            .end()
-            .then(function(){
-            	console.log("done")
+// if mobile config.mobileuseragent || 
+var action = function(user, platformConfig) {
+    rp(config.wordsEndpoint)
+        .then(function(wordList) {
+            console.log("user" + JSON.stringify(user))
+            wordList = JSON.parse(wordList)
+            words = [];
+            wordList.forEach(function(item) {
+                words.push(item.word)
             })
+        })
+        .then(function() {
+            var nightmare = new Nightmare({
+                    show: true,
+                    'webPreferences': {
+                        partition: 'nopersist'
+                    }
+                })
+                // give it a desktop user agent mac
+            nightmare
+                .useragent(platformConfig.userAgent)
+                .viewport(1000, 700)
+                .goto(config.url)
+                .click(platformConfig.goToSignIn)
+                .wait(1500)
+                .type('input[type="email"]', user.email)
+                .type('input[type="password"]', user.password)
+                .click(config.signInSelector)
+                .wait(1300)
+                .goto(config.homeUrl)
+                .wait(1400)
+            for (var i = 0; i < words.length; i++) {
+                nightmare
+                    .insert('input[type="search"]', '')
+                    .type('input[type="search"]', words[i])
+                    .wait(500)
+                    .type('document', '\u000d')
+                    .click(config.searchSelector)
+                    .wait(1500)
+            }
+            nightmare
+                .wait(3000)
+                .end()
+                .then(function() {
+                    doit++
+                    if (users.length > doit) desktop(users[doit]);
+                    else doit = 0;
+                })
+                .catch(function(err) {
+                    console.log("err " + err)
+                })
 
-    })
+        })
 }
