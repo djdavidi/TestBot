c4fc0d4795366f6dea83ef183de92464d0def990
[David Scherban] <davidscherban@gmail.com>
2016-07-16 02:44:26 -0400

    removed unnecessary code, added semicolons

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # TestBot
 Search bot using nightmare.js for movement and actions through webpages, request promise and wordnik API for search term list,
-and cron to schedule the bots running time
+and cron to schedule the bots running time.
--- app.js
+++ app.js
@@ -2,45 +2,34 @@ var CronJob = require('cron').CronJob;
 var config = require('./config')
 var Nightmare = require('nightmare');
 var rp = require('request-promise');
-var randomNumber;
-var user;
 var words;
-var indexPointer = Math.floor(Math.random()*5);;
-var counter = 0;
-var platform;
 
-var job = new CronJob('* * 8-18/1 * *', function() {
-    if(counter<=4) platform = config.desktop;
-    else if(counter>4) platform = config.mobile;
-    action(config.users[indexPointer],platform);
-    indexPointer++;
-    counter++;
-    if(counter===10)counter = 0;
-    if(indexPointer===config.users.length) indexPointer=0;
 
+var job1 = new CronJob('* 30 * * * *', function() {
+    search(config.users[doit], config.desktop);
 }, null, true,'America/New_York')
 
-var action = function(user, platformConfig) {
+var search = function(user, platformConfig) {
     rp(config.wordsEndpoint)
         .then(function(wordList) {
-            console.log("user" + JSON.stringify(user))
             wordList = JSON.parse(wordList)
             words = [];
             wordList.forEach(function(item) {
-                words.push(item.word)
-            })
+                words.push(item.word);
+            });
         })
         .then(function() {
             var nightmare = new Nightmare({
-                    show: true,
-                    'webPreferences': {
-                        partition: 'nopersist'
-                    }
-                })
+                show: true,
+                'webPreferences': {
+                    partition: 'nopersist'
+                }
+            })
             nightmare
                 .useragent(platformConfig.userAgent)
                 .viewport(1000, 700)
                 .goto(config.url)
+                .wait(1000)
                 .click(platformConfig.goToSignIn)
                 .wait(1500)
                 .type('input[type="email"]', user.email)
@@ -53,19 +42,13 @@ var action = function(user, platformConfig) {
                 nightmare
                     .insert('input[type="search"]', '')
                     .type('input[type="search"]', words[i])
-                    .wait(500)
-                    .type('document', '\u000d')
-                    .click(config.searchSelector)
+                    .wait(1000)
+                    .type('input[type="search"]', '\u000d')
                     .wait(1500)
             }
             nightmare
                 .wait(3000)
                 .end()
-                .then(function() {
-                    doit++
-                    if (users.length > doit) desktop(users[doit]);
-                    else doit = 0;
-                })
                 .catch(function(err) {
                     console.log("err " + err)
                 })
